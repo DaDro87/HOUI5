dfb39bf9bb1a6988dda9497886bdfb9db1838ba8
bas02 clouddnagmbhgmail.onmicrosoft.com <bas02@clouddnagmbhgmail.onmicrosoft.com>
2023-11-27 14:41:22 +0000

    Initial Commit

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("at.clouddna.training02.zhoui5.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"atclouddnatraining02/zhoui5/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
